422a3f1e176cf1d26ea4c24a528341e08205b11d
Seany <102695597+Seanii86@users.noreply.github.com>
2023-01-23 22:20:56 -0800

    'Navbar'

---
+++ src/Client/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Navbar'
+
+
+export default function Home() {
+    return (
+        <>
+        <Navbar />
+            <div>
+                Homepage
+            </div>
+        </>
+    )
+}
